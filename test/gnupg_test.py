fd882f65800aedb4486817099286e4be7096e04e
Micah Lee <micah.lee@theintercept.com>
2015-09-12 22:17:39 -0700

    Implemented tests: test_gpg_test_key_not_found_in_keyring, test_gpg_test_key_revoked, test_gpg_test_key_expired

--- test/gnupg_test.py
+++ test/gnupg_test.py
@@ -7,8 +7,8 @@ test_key_fp = 'ABCFD99FA1617E55B8CDE5ADE36FD670777947EB'
 gpg_homedir = os.path.abspath('test/homedir')
 def get_gpg_file(filename):
     return os.path.join(os.path.abspath('test/gpg_files'), filename)
-def import_test_key():
-    subprocess.call(['gpg', '--homedir', gpg_homedir, '--import', get_gpg_file('pgpsync_test_suite_pubkey.asc')],
+def import_key(filename):
+    subprocess.call(['gpg', '--homedir', gpg_homedir, '--import', get_gpg_file(filename)],
         stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
 
@@ -44,8 +44,24 @@ def test_gpg_recv_key_not_found_on_keyserver():
     gpg.recv_key('hkp://keys.gnupg.net', '0000000000000000000000000000000000000000')
 
 @with_setup(setup_func)
-def test_gpg_test_key():
-    pass
+@raises(NotFoundInKeyring)
+def test_gpg_test_key_not_found_in_keyring():
+    gpg = GnuPG(homedir=gpg_homedir)
+    gpg.test_key('0000000000000000000000000000000000000000')
+
+@with_setup(setup_func)
+@raises(RevokedKey)
+def test_gpg_test_key_revoked():
+    gpg = GnuPG(homedir=gpg_homedir)
+    import_key('pgpsync_revoked_pubkey.asc')
+    gpg.test_key('3B4D CA7D CCE8 25F0 5E34  0C83 08C6 62C0 2BA4 39DE')
+
+@with_setup(setup_func)
+@raises(ExpiredKey)
+def test_gpg_test_key_expired():
+    gpg = GnuPG(homedir=gpg_homedir)
+    import_key('pgpsync_expired_pubkey.asc')
+    gpg.test_key('D9C4 DB9D E638 B5CD F1C4  CE17 DB54 DE95 978A 4EB0')
 
 @with_setup(setup_func)
 def test_gpg_get_uid():
