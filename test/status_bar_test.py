f00f1f169aa0990e53dede8b9a2dd1183406845c
Micah Lee <micah.lee@theintercept.com>
2015-09-12 20:14:10 -0700

    Wrote MessageQueue tests

--- test/status_bar_test.py
+++ test/status_bar_test.py
@@ -12,3 +12,25 @@ def test_status_bar_hide_loading_animation():
     status_bar = StatusBar()
     status_bar.hide_loading()
     assert status_bar.loading_animation.isHidden()
+
+def test_message_queue_add_message():
+    q = MessageQueue()
+    q.add_message('this is a test')
+    q.add_message('another test', timeout=2000)
+    q.add_message(type="clear")
+
+    assert q.get(False) == {
+        'type': 'update',
+        'msg': 'this is a test',
+        'timeout': 0
+    }
+    assert q.get(False) == {
+        'type': 'update',
+        'msg': 'another test',
+        'timeout': 2000
+    }
+    assert q.get(False) == {
+        'type': 'clear',
+        'msg': None,
+        'timeout': 0
+    }
