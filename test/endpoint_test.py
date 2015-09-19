794892a5034e736db84250f3ac2b226bbaa43313
Micah Lee <micah.lee@theintercept.com>
2015-09-18 17:57:21 -0700

    Added stubs for Endpoint tests

---
+++ test/endpoint_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from nose import with_setup
+from nose.tools import raises
+from pgpsync import *
+
+from .test_helpers import *
+
+def test_fetch_url_valid_url():
+    pass
+
+def test_fetch_url_invalid_url():
+    pass
+
+def test_fetch_url_valid_url_invalid_proxy():
+    pass
+
+def test_fetch_url_valid_url_valid_proxy():
+    pass
+
+def test_get_fingerprint_list_valid():
+    # Test without whitespace, with whitespace, and with whitespace and comments
+    pass
+
+def test_get_fingerprint_list_invalid():
+    # Test with lines that are not valid fingerprints
+    pass
