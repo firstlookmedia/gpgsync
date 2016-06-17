ba2daf1d38bbdb69555e68e8b08a17c905b78913
Sam Couch <samcouch91@gmail.com>
2016-06-17 10:23:53 -0400

    add clean flag to ignore the pyinstaller cache

--- install/build_osx.sh
+++ install/build_osx.sh
@@ -8,7 +8,7 @@ rm -rf $ROOT/dist &>/dev/null 2>&1
 
 # Build the .app
 echo Building .app
-pyinstaller install/pyinstaller-osx.spec
+pyinstaller install/pyinstaller-osx.spec --clean
 
 if [ "$1" = "--sign" ]; then
   echo "Codesigning is not yet implemented"
