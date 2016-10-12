2dc541cf05bb727367a29455335748ecadd1368c
Leo Arias <yo@elopio.net>
2016-10-12 17:09:24 +0000

    Fix the path to the version file.
    Closes #76.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os, sys, platform
 
 p = platform.system()
-version_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'share', 'version')
+version_file = os.path.join(os.path.dirname(__file__), 'share', 'version')
 version = open(version_file).read().strip().lstrip('v')
 
 description = "GPG Sync lets users have up-to-date public keys for all other members of their organization."
