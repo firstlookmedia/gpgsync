c18b2a24ae76ce52432335400eae06cd03307535
Micah Lee <micah@micahflee.com>
2017-02-07 16:51:21 -0800

    Oops, import requests again

--- gpgsync/endpoint.py
+++ gpgsync/endpoint.py
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import socks, uuid, datetime
+import requests, socks, uuid, datetime
 import dateutil.parser as date_parser
 from io import BytesIO
 from PyQt5 import QtCore, QtWidgets
