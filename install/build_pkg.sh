c77d8c406970af468d2f4bd27e7a8677de8d9b44
Micah Lee <micah@micahflee.com>
2019-03-12 17:30:28 -0700

    Update macOS package script to allow --release-without-codesigning, and to not create a component, but rather add scripts directly to the product

--- install/build_pkg.sh
+++ install/build_pkg.sh
@@ -2,24 +2,23 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 BUILD_PATH="$ROOT/build"
 DIST_PATH="$ROOT/dist"
+SCRIPTS_PATH="$ROOT/install/macos-packaging/scripts"
+
+# Get the version
+VERSION=$(cat share/version)
+VERSION=${VERSION:1}
 
 cd $ROOT
 
-# Delete old build
-echo Deleting dist folder
+echo "Deleting older builds"
 rm -rf $BUILD_PATH $DIST_PATH &>/dev/null 2>&1
 
-# Get the version
-VERSION=$(cat share/version | cut -d"v" -f2)
-
-# Build the .app
-echo Building GPG Sync.app
+echo "Building the app bundle"
 pyinstaller install/pyinstaller.spec --clean
 
 if [ "$1" = "--release" ]; then
   APP_PATH="$DIST_PATH/GPG Sync.app"
-  PKG_PATH="$DIST_PATH/GPGSync.pkg"
-  PRODUCT_PATH="$DIST_PATH/GPGSync-$VERSION.pkg"
+  PKG_PATH="$DIST_PATH/GPGSync-$VERSION.pkg"
   IDENTITY_NAME_APPLICATION="Developer ID Application: FIRST LOOK PRODUCTIONS, INC."
   IDENTITY_NAME_INSTALLER="Developer ID Installer: FIRST LOOK PRODUCTIONS, INC."
 
@@ -27,11 +26,23 @@ if [ "$1" = "--release" ]; then
   codesign --deep -s "$IDENTITY_NAME_APPLICATION" "$APP_PATH"
 
   echo "Creating an installer"
-  pkgbuild --sign "$IDENTITY_NAME_INSTALLER" --root "$DIST_PATH" --component-plist install/macos-packaging/gpgsync-component.plist --scripts install/macos-packaging/scripts "$PKG_PATH"
-  productbuild --sign "$IDENTITY_NAME_INSTALLER" --package "$PKG_PATH" "$PRODUCT_PATH"
+  productbuild --sign "$IDENTITY_NAME_INSTALLER" --scripts "$SCRIPTS_PATH" --component "$APP_PATH" /Applications "$PKG_PATH"
 
   echo "Cleaning up"
-  rm -rf "$APP_PATH" "$PKG_PATH"
+  rm -rf "$APP_PATH"
 
-  echo "All done, your installer is in: $PRODUCT_PATH"
+  echo "All done, your installer is in: $PKG_PATH"
+fi
+
+if [ "$1" = "--release-without-codesigning" ]; then
+  APP_PATH="$DIST_PATH/GPG Sync.app"
+  PKG_PATH="$DIST_PATH/GPGSync-$VERSION.pkg"
+
+  echo "Creating an installer"
+  productbuild --scripts "$SCRIPTS_PATH" --component "$APP_PATH" /Applications "$PKG_PATH"
+
+  echo "Cleaning up"
+  rm -rf "$APP_PATH"
+
+  echo "All done, your installer is in: $PKG_PATH"
 fi
